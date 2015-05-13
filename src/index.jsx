3ec81fa1b30cfdfdb1acb75a2e4158eff3e863d0
Markus Greystone <markus.greystone@gmail.com>
2015-05-13 10:43:28 -0400

    Update 2015-05-13T14:43:27.378Z

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-import React from 'react'
-import Router from 'react-router'
-
-import routes from './routes'
-
-Router.run(routes, Handler => React.render(<Handler />, document.body))
