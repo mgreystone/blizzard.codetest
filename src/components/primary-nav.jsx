c479472c8dce404e787391f8c9f6d855c2c58e0c
Markus Greystone <markus.greystone@gmail.com>
2015-05-16 21:03:24 -0400

    Add login & logout link.

--- src/components/primary-nav.jsx
+++ src/components/primary-nav.jsx
@@ -3,12 +3,26 @@
 import './primary-nav.scss'
 
 import React from 'react'
+import Reflux from 'reflux'
 import { Link } from 'react-router'
+
+import authenticationActions from '../actions/authentication'
+import authenticationStore from '../stores/authentication'
 
 const PrimaryNav = React.createClass({
   displayName: 'PrimaryNav',
 
+  mixins: [
+    Reflux.connect(authenticationStore, 'authentication')
+  ],
+
+  onClickLogin () {
+    authenticationActions.authenticate()
+  },
+
   render () {
+    let isAuthenticated = !!this.state.authentication.accountId
+
     return (
       <nav className='primary-nav'>
         <ul>
@@ -17,6 +31,11 @@ const PrimaryNav = React.createClass({
           </li>
           <li>
             <Link to='tags'>Tags</Link>
+          </li>
+          <li>
+            {isAuthenticated ?
+              <span className='link' onClick={this.onClickLogin}>Login</span> :
+              <span className='link'>Logout</span>}
           </li>
         </ul>
       </nav>
